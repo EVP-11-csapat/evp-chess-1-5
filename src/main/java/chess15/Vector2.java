780d253b1d0b95d7134a7934c824c6975bdb4520
Exclusivefrog28 <deee4444@gmail.com>
2022-11-04 13:45:09 +0100

    Vector2 changes

--- src/main/java/chess15/Vector2.java
+++ src/main/java/chess15/Vector2.java
@@ -1,40 +1,22 @@
 package chess15;
 
 public class Vector2 {
-    private int x;
-    private int y;
+    public int x;
+    public int y;
 
     public Vector2(int x, int  y){this.x = x; this.y = y;}
-    public Vector2(Vector2 a){this.x = a.getX(); this.y = a.getY();}
 
-    public int getY(){return this.y;}
-    public int getX(){return this.x;}
-    public void setX(int a){this.x = a;}
-    public void setY(int a){this.y = a;}
-
-    public Vector2 add(Vector2 a)
-    {
-        this.x += a.getX();
-        this.y += a.getY();
-        return this;
-    }
-    public Vector2 subtract(Vector2 a)
-    {
-        this.x -= a.getX();
-        this.y -= a.getY();
-        return this;
-    }
     public static Vector2 add(Vector2 a, Vector2 b)
     {
-        return new Vector2(a.getY()+b.getY(), a.getX() + b.getX());
+        return new Vector2(a.x + b.x, a.y + b.y);
     }
-    public static Vector2 subtract(Vector2 a, Vector2 b)
-    {
-        return new Vector2(a.getY()-b.getY(), a.getX() - b.getX());
+
+    public Vector2 inverse(){
+        return new Vector2(-this.x, -this.y);
     }
 
     @Override
     public String toString() {
-        return ("(x: "+this.x+", y: "+this.y+")");
+        return ("( "+this.x+", "+this.y+" )");
     }
 }
