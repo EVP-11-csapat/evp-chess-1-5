83b00907e7d88bbe69ecff1ff9279a69cfbc19cc
Exclusivefrog28 <deee4444@gmail.com>
2022-11-04 14:17:53 +0100

    fixed Board.getElement()

--- src/main/java/chess15/Board.java
+++ src/main/java/chess15/Board.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Board {
     public BoardElement[][] elements = new BoardElement[8][8];
 
-    public BoardElement getElement(int row, int col){
+    public BoardElement getElement(int col, int row){
         // column is a number too, (1,1) translates to a1, while keeping the implementation in the correct orientation
         return elements[col - 1][8 - row];
     }
