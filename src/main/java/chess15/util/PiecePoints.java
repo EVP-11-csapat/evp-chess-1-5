af5b830104120118d37240893084bed3b0f2b2dc
Németh Tamás <34923997+Nemeth-Tamas@users.noreply.github.com>
2022-12-22 15:22:10 +0100

    Changed PiecePoints from look to movement class

--- src/main/java/chess15/util/PiecePoints.java
+++ src/main/java/chess15/util/PiecePoints.java
@@ -1,15 +1,14 @@
 package chess15.util;
 
-import chess15.Piece;
+import chess15.*;
 
 public class PiecePoints {
     public static int evaluate(Piece p) {
-        return switch (p.look) {
-            case PAWN -> 1;
-            case ROOK -> 5;
-            case KNIGHT, BISHOP -> 3;
-            case QUEEN -> 9;
-            default -> 0;
-        };
+        if (p.movement.getClass() == Pawn.class) return 1;
+        if (p.movement.getClass() == Knight.class) return 3;
+        if (p.movement.getClass() == Bishop.class) return 3;
+        if (p.movement.getClass() == Rook.class) return 5;
+        if (p.movement.getClass() == Queen.class) return 9;
+        else return 0;
     }
 }
